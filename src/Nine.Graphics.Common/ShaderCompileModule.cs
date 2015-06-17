951601eab7abf10cc5ca37bb521d5e00c49f39cc
Yufei Huang <yufeih@live.com>
2015-06-17 21:16:53 +0800

    Basic ShaderCompileModule

---
+++ src/Nine.Graphics.Common/ShaderCompileModule.cs
@@ -0,0 +1,32 @@
+﻿#if DX
+namespace Nine.Graphics.DirectX
+#else
+namespace Nine.Graphics.OpenGL
+#endif
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+    using System.Threading;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.Framework.Runtime.Roslyn;
+
+    public partial class ShaderCompileModule : ICompileModule
+    {
+        public static byte[] GetByteCode(string shaderPath)
+        {
+            throw new InvalidOperationException($"This method should have been replaced by { nameof(ShaderCompileModule) }");
+        }
+
+        public void AfterCompile(IAfterCompileContext context) { }
+        public void BeforeCompile(IBeforeCompileContext context)
+        {
+            foreach (var syntaxTree in context.Compilation.SyntaxTrees)
+            {
+
+            }
+
+            context.Compilation = context.Compilation;
+        }
+    }
+}
---
+++ src/Nine.Graphics.DirectX/Compile/Preprocess/ShaderCompileModule.DirectX.cs
@@ -0,0 +1,10 @@
+﻿namespace Nine.Graphics.DirectX
+{
+    partial class ShaderCompileModule
+    {
+        private byte[] PlatformCompileShader(string content, string path)
+        {
+            return new byte[0];
+        }
+    }
+}
