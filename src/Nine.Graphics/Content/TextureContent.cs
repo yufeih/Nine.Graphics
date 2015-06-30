7885f14e9d75addfd1f0c4bacf46cfd46ca33393
Yufei Huang <yufeih@live.com>
2015-06-30 17:12:52 +0800

    Add left/right/top/bottom property to TextureContent class so we can load texture atlas independent of the platform.

--- src/Nine.Graphics/Content/TextureContent.cs
+++ src/Nine.Graphics/Content/TextureContent.cs
@@ -1,23 +1,41 @@
 ﻿namespace Nine.Graphics.Content
 {
+    using System;
     using System.Diagnostics;
 
     public class TextureContent
     {
         public readonly int Width;
         public readonly int Height;
+
         public readonly byte[] Pixels;
+
+        public readonly int Left;
+        public readonly int Right;
+        public readonly int Top;
+        public readonly int Bottom;
+
         public readonly bool IsTransparent;
 
-        public TextureContent(int width, int height, byte[] pixels, bool isTransparent = true)
+        public TextureContent(
+            int width, int height, byte[] pixels, bool isTransparent = true,
+            int left = 0, int? right = null, int top = 0, int? bottom = null)
         {
+            if (pixels == null) throw new ArgumentNullException(nameof(pixels));
+
             Debug.Assert(width > 0);
             Debug.Assert(height > 0);
-            Debug.Assert(pixels != null);
+
+            this.Pixels = pixels;
 
             this.Width = width;
             this.Height = height;
-            this.Pixels = pixels;
+
+            this.Left = left;
+            this.Top = top;
+            this.Right = right != null ? right.Value : width;
+            this.Bottom = bottom != null ? bottom.Value : height;
+
             this.IsTransparent = isTransparent;
         }
 
