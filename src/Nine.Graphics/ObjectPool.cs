12c16aba1890ea9ca08c2078df10a1749c7e8e29
Yufei Huang <yufeih@live.com>
2015-06-30 16:58:13 +0800

    Make Sprite a primitive drawing type by removing unnessessary properties. Add ISpriteRenderer to draw sprites with various configurations. Implement sprite batching by textures.

--- src/Nine.Graphics/ObjectPool.cs
+++ src/Nine.Graphics/ObjectPool.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using Nine.Graphics.Primitives;
 
     public class ObjectPool
     {
---
+++ src/Nine.Graphics/Primitives/Sprite.cs
@@ -0,0 +1,36 @@
+﻿namespace Nine.Graphics.Primitives
+{
+    using Nine.Imaging;
+    using System.Numerics;
+
+    public struct Sprite
+    {
+        public readonly Vector2 Position;
+        public readonly Vector2 Scale;
+        public readonly Vector2 Size;
+        public readonly float Rotation;
+        public readonly Vector2 Origin;
+        public readonly TextureId Texture;
+        public readonly Color Color;
+
+        public static readonly Sprite Default = new Sprite(default(TextureId));
+        
+        public Sprite(
+            TextureId texture,
+            Vector2 position = default(Vector2),
+            Vector2? scale = null,
+            Vector2? size = null,
+            float rotation = 0,
+            Vector2 origin = default(Vector2),
+            Color? color = null)
+        {
+            this.Texture = texture;
+            this.Position = position;
+            this.Scale = scale ?? Vector2.One;
+            this.Rotation = rotation;
+            this.Size = size ?? Vector2.Zero;
+            this.Origin = origin;
+            this.Color = color ?? Color.White;
+        }
+    }
+}
