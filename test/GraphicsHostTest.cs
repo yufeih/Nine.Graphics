9a31572a61e0d4c82cd5dc95bcd6951daa518bcc
Yufei Huang <yufeih@live.com>
2016-06-26 19:45:29 +0800

    GraphicsHostTest.empty_scene

---
+++ test/GraphicsHostTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Nine.Graphics
+{
+    using System;
+    using Xunit;
+
+    public class GraphicsHostTest
+    {
+        public static readonly DrawingTheoryData<string> Scenes = new DrawingTheoryData<string> { { "" } };
+
+        [Theory, MemberData(nameof(Scenes))]
+        public static void empty_scene(Lazy<DrawingContext> contextFactory, string scene)
+        {
+            var context = contextFactory.Value;
+            if (context == null) return;
+
+            context.DrawFrame<GraphicsHostTest>((width, height) => { });
+        }
+    }
+}
